575b85ca7d60d43087af519696f248a34015f373
Navin Developer <dev@navin.local>
2026-04-24 19:48:14 +0100

    feat: sync legacy src folder with packages/core changes

--- src/index.ts
+++ src/index.ts
@@ -36,8 +36,16 @@ export type { WalletConnector } from './wallet/walletConnector';
 // Utils
 export { ConcurrencyQueue, createConcurrencyControlledClient } from './utils/concurrencyQueue';
 export { retry, createHttpClientWithRetry } from './utils/httpInterceptor';
-export { buildContractCallOperation, buildContractCallTransaction, toScVal } from './utils/transactionBuilder';
+export { buildContractCallOperation, buildContractCallTransaction, buildBaseTransaction, toScVal } from './utils/transactionBuilder';
+export type { BuildBaseTransactionParams } from './utils/transactionBuilder';
 export { getDefaultRpcUrl, getNetworkPassphrase, resolveNetworkConfig } from './utils/networkConfig';
+export { parseTransaction, sortByTimestamp, filterByActionType } from './utils/transactionHistory';
+export type {
+    TransactionActionType,
+    TransactionHistoryEntry,
+    FetchTransactionHistoryOptions,
+    TransactionHistoryResult
+} from './utils/transactionHistory';
 
 // Testing & MSW
 export * from './test/msw/setup';
