af166a8e3b667d35e8269daf8ac16aa7698579ac
Oyeleke Amir <oyelekeamir123@gmail.com>
2026-08-28 20:17:55 +0100

    feat: implemented a comprehensive schema for SDK Soroban transaction

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,6 +3,26 @@ export * from './contracts/vault';
 export * from './soroban';
 export * from './errors';
 export * from './events';
+export {
+  ExecutionNetwork,
+  SorobanExecutionRequest,
+  SorobanExecutionResult,
+  SorobanExecutionRequestInput,
+  buildSorobanExecutionRequest,
+  executionSuccess,
+  executionFailed,
+  executionPending,
+  validateSorobanExecutionResult
+} from './execution';
+export {
+  validateExecutionNetwork,
+  validateSimulationResult,
+  validateSorobanExecutionRequestSchema,
+  validateSorobanExecutionResultSchema,
+  isExecutionNetwork,
+  isSorobanExecutionRequest,
+  isSorobanExecutionResult
+} from './executionSchemas';
 export * from './network';
 export * from './testing';
 export * from './transactions';
